3b5d7438a43904e175feda5be6169293048bc9b7
werahul <rahuldev.kb@gmail.com>
2025-08-29 16:20:08 +0530

    db

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json()); 
 
 
-mongoose.connect(process.env.MONGO_URI || "mongodb://127.0.0.1:27017/collab-notes")
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
   .catch(err => console.error("❌ Mongo error:", err));
 
